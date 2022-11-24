6ba559e21b54a0f7239f90a32e7aa2f2608392e6
guzin-dev <guzinhojk99@gmail.com>
2022-11-24 20:57:09 -0300

    Projeto terminado

--- script.js
+++ script.js
@@ -6,14 +6,24 @@ let inseridoPopup = document.getElementById("inserido-popup");
 let vazio = document.getElementById("listaVazia");
 let preco = document.getElementById("preco");
 
+window.onload = carregarLocalStorage; //Quando a pessoa recarregar a página vai carregar todas os itens que ela já havia criado
+
 btnInserir.addEventListener("click", () => {
   inserirItem();
 });
 
-function inserirItem() {
-  if (produtoInput.value.length < 8) {
-    alert("Insira no mímimo oito caracteres!");
-  } else {
+function carregarLocalStorage() {
+  if (!localStorage.length < 1) {
+    for (var i = 0; i < localStorage.length; i++) {
+      if (localStorage.key(i).startsWith("COMPRA_")) {
+        criar_carregarItem("carregar", i);
+      }
+    }
+  }
+}
+
+function criar_carregarItem(criar_carregar, carregar_index) {
+  if (criar_carregar == "criar") { //Vai criar os items
     vazio.remove(); //Apaga o texto "vazia"
     let div = document.createElement("div"); //Div pai
     let div2 = document.createElement("div"); //Div filha
@@ -31,49 +41,121 @@ function inserirItem() {
     deleteButton.appendChild(buttonText); //Adiciona o "x" do botão de deletar
     lista.appendChild(div); //Adiciona a div pai na div lista
 
-    inserirLocalStorage(produtoInput.value); //Insere o produto no LocalStorage
+    let produtoObj = { nome: produtoInput.value, valor: 0 }; //Cria o objeto do produto
+    let produtoObj_serialized = JSON.stringify(produtoObj); //Serializa o objeto pra inserir no LocalStorage
+
+    inserirLocalStorage(produtoInput.value, produtoObj_serialized); //Insere o produto no LocalStorage
+
+    let produtoValue = produtoInput.value;
 
     checkbox.addEventListener("change", () => {
-      inserirValor(checkbox, div2); //Cria a função de adicionar valor a um produto
+      inserirValor(checkbox, produtoValue); //Cria a função de adicionar valor a um produto
     });
 
     deleteButton.addEventListener("click", (e) => {
-      removerItem(e); //Cria a função de apagar um item da lista/LocalStorage
+      removerItem(e, checkbox); //Cria a função de apagar um item da lista/LocalStorage
     });
 
     inserirAnimation(); //Faz a animação de item adicionado
+  } else if (criar_carregar == "carregar") { //Vai carregar todos os itens
+    let itemObj_deserialized = JSON.parse(
+      localStorage.getItem(localStorage.key(carregar_index))
+    );
+
+    vazio.remove();
+    let div = document.createElement("div"); //Div pai
+    let div2 = document.createElement("div"); //Div filha
+    let checkbox = document.createElement("input"); //Checkbox do produto
+    let item = document.createTextNode(itemObj_deserialized.nome); //Nome do produto
+    let deleteButton = document.createElement("button"); //Botão de apagar
+    let buttonText = document.createTextNode("x"); //"x" do botão de apagar
+    checkbox.type = "checkbox"; //Checkbox agora virou checkbox, antes era input normal
+    div.classList.add("item"); //Adiciona a classe item pra div pai
+    div2.classList.add("items-aligner"); //Adiciona a classe items-aligner na div filha
+    div.appendChild(div2); //Adiciona a div filha na div pai
+    div2.appendChild(checkbox); //Adiciona a checkbox na div filha
+    div2.appendChild(item); //Adiciona o nome do produto na div filha
+    div.appendChild(deleteButton); //Adiciona o botão de deletar na div filha
+    deleteButton.appendChild(buttonText); //Adiciona o "x" do botão de deletar
+    lista.appendChild(div); //Adiciona a div pai na div lista
+
+    if (itemObj_deserialized.valor != 0) {
+      checkbox.checked = true;
+      let total = preco.innerText;
+      total = parseFloat(total);
+
+      let antigoProduto = localStorage.getItem(
+        `COMPRA_${itemObj_deserialized.nome}`
+      );
+      let antigoProduto_deserialized = JSON.parse(antigoProduto);
+      valor = antigoProduto_deserialized.valor;
+      checkbox.value = valor;
+
+      total += parseFloat(valor);
+      preco.innerText = total.toFixed(2);
+    }
+
+    let a = itemObj_deserialized.nome;
+
+    checkbox.addEventListener("change", () => {
+      inserirValor(checkbox, a); //Cria a função de adicionar valor a um produto
+    });
+
+    deleteButton.addEventListener("click", (e) => {
+      removerItem(e, checkbox);
+    });
   }
 }
 
-function inserirLocalStorage(param) {
+function inserirItem() {
+  if (produtoInput.value.length < 8) {
+    alert("Insira no mímimo oito caracteres!");
+  } else {
+    criar_carregarItem("criar");
+  }
+}
+
+function inserirLocalStorage(produto, produtoObj_serialized) {
   for (let i = 0; i < localStorage.length; i++) {
     if (localStorage.getItem(i) == undefined) {
-      localStorage.setItem(`COMPRA_${param}`, param);
+      localStorage.setItem(`COMPRA_${produto}`, produtoObj_serialized);
       break;
     }
   }
 }
 
-function inserirValor(param, param2) {
+function inserirValor(checkbox, produto) {
   let total = preco.innerText;
   total = parseFloat(total);
-  if (param.checked) {
+
+  let antigoProduto = localStorage.getItem(`COMPRA_${produto}`);
+  let antigoProduto_deserialized = JSON.parse(antigoProduto);
+
+  if (checkbox.checked) {
     let valor = prompt("Insira o preço do produto");
     if (valor != null && valor != 0 && valor > 0) {
-      var taxado = document.createElement("div")
-      taxado.classList.add("taxado")
-      param2.appendChild(taxado)
       total += parseFloat(valor);
       preco.innerText = total.toFixed(2);
-      param.value = valor;
+      checkbox.value = valor;
+
+      antigoProduto_deserialized.valor = valor;
+      antigoProduto = JSON.stringify(antigoProduto_deserialized);
+      localStorage.removeItem(`COMPRA_${produto}`);
+      localStorage.setItem(`COMPRA_${produto}`, antigoProduto);
     } else {
       alert("Erro ao inserir valor, por favor tente novamente!");
-      param.checked = false;
+      checkbox.checked = false;
     }
   } else {
-    param2.querySelector(".taxado").remove()
-    total = total - parseFloat(param.value);
+    total = total - parseFloat(checkbox.value);
     preco.innerText = total.toFixed(2);
+    checkbox.value = checkbox.value - checkbox.value;
+
+    antigoProduto_deserialized.valor =
+      antigoProduto_deserialized.valor - antigoProduto_deserialized.valor;
+    antigoProduto = JSON.stringify(antigoProduto_deserialized);
+    localStorage.removeItem(`COMPRA_${produto}`);
+    localStorage.setItem(`COMPRA_${produto}`, antigoProduto);
   }
 }
 
@@ -82,64 +164,20 @@ function inserirAnimation() {
   produtoInput.value = "";
 }
 
-function removerItem(param) {
-  const pai = param.target.parentNode;
+function removerItem(e, checkbox) {
+  const pai = e.target.parentNode;
   const items_aligner = pai.querySelector(".items-aligner");
   localStorage.removeItem(`COMPRA_${items_aligner.innerText}`);
-  pai.remove();
-}
-
-window.onload = carregarLocalStorage; //Quando a pessoa recarregar a página vai carregar todas os itens que ela já havia criado
-
-function carregarLocalStorage() {
-  if (!localStorage.length < 1) {
-    for (var i = 0; i < localStorage.length; i++) {
-      if (localStorage.key(i).startsWith("COMPRA_")) {
-        vazio.remove();
-        var div = document.createElement("div");
-        var div2 = document.createElement("div");
-        var checkbox = document.createElement("input");
-        var item = document.createTextNode(
-          localStorage.getItem(localStorage.key(i))
-        );
-        var button = document.createElement("button");
-        var buttonText = document.createTextNode("x");
-        checkbox.type = "checkbox";
-        checkbox.classList.add("checkbox");
-        div.classList.add("item");
-        div2.classList.add("itens-aligner");
-        div.appendChild(div2);
-        div2.appendChild(checkbox);
-        div2.appendChild(item);
-        div.appendChild(button);
-        button.appendChild(buttonText);
-        lista.appendChild(div);
-
-        checkbox.addEventListener("change", () => {
-          var total = preco.innerText;
-          var valNumber = parseFloat(total);
-          if (checkbox.checked) {
-            var valor = prompt("Insira o valor");
-            valNumber += parseFloat(valor);
-            preco.innerText = valNumber.toFixed(2);
-            checkbox.value = valor;
-          } else {
-            valNumber = valNumber - parseFloat(checkbox.value);
-            preco.innerText = valNumber.toFixed(2);
-          }
-        });
-
-        button.addEventListener("click", (e) => {
-          for (let j = 0; j < localStorage.length; j++) {
-            const pai = e.target.parentNode;
-            const itens_aligner = pai.querySelector(".itens-aligner");
-            if (localStorage.getItem(localStorage.key(j)) == itens_aligner.innerText) {
-              localStorage.removeItem(localStorage.key(j));
-              pai.remove();
-            }
-          }
-        });
-      }
-    }
+  if (
+    checkbox.value != undefined &&
+    checkbox.value != null &&
+    checkbox.value != 0 &&
+    checkbox.value != "on"
+  ) {
+    let total = preco.innerText;
+    total = parseFloat(total);
+    total = total - parseFloat(checkbox.value);
+    preco.innerText = total.toFixed(2);
   }
+  pai.remove();
 }
